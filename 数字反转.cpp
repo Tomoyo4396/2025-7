e9e5ff3fd876768d64592263fbce1e0a2ed5fa07
Tomoyo4396 <463659267@qq.com>
2025-07-22 18:17:53 +0800

    Add files via upload

--- 数字反转.cpp
+++ 数字反转.cpp
@@ -1,11 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string>
+#include<algorithm>
 
 int main()
 {
-	char A[100];
-	scanf("%s", &A);
+	char A[10];
+	scanf("%s", A);
 	std::string a(A);//构造函数将char[]构造成std::string
 	std::reverse(a.begin(), a.end());
 	printf("%s", a.c_str());
